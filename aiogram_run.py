739dfe838eac8d527d39278e7971750a1a440782
laktes1 <laktes1@gmail.com>
2024-11-14 17:50:28 +0500

    readme

--- aiogram_run.py
+++ aiogram_run.py
@@ -1,6 +1,5 @@
 import asyncio
 import os
-from dotenv import load_dotenv
 from create_bot import bot, dp, scheduler
 from handlers.start import start_router
 # from work_time.time_func import send_time_msg
@@ -14,6 +13,3 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
-    dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-    if os.path.exists(dotenv_path):
-        load_dotenv(dotenv_path)
